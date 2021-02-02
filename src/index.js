7e34148c7d07e97e969f9aeb680a8cdd1b86d97c
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2021-02-02 18:47:00 -0300

    componente na forma mais simples

--- src/index.js
+++ src/index.js
@@ -2,18 +2,9 @@ import './index.css'
 import ReactDom from 'react-dom'
 import React from 'react'
 
-import Primeiro from './components/basicos/Primeiro'
-import ComParametro from './components/basicos/ComParametro'
-import Fragmento from './components/basicos/Fragmento'
+import App from './App'
 
 ReactDom.render(
-    <div id="app">
-        <Primeiro></Primeiro>
-        <ComParametro
-            titulo="Situação do Aluno"
-            aluno="Pedro Silva"
-            nota={9.3} />
-        <Fragmento />
-    </div>,
+    <App />,
     document.getElementById('root')
 )
