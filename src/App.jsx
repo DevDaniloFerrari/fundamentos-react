c7737bf231522480d3e77c992c6b2af12cfbb8ee
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2021-02-04 06:29:31 -0300

    aplicando flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
@@ -7,24 +8,28 @@ import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
 
 export default () =>
-    <div id="app">
+    <div className="App">
         <h1>Fundamento React</h1>
-        <Card titulo="#04 - Desafio Aleatório">
-            <Aleatorio min={5} max={15} />
-        </Card>
 
-        <Card titulo="#03 - Fragmento">
-            <Fragmento />
-        </Card>
+        <div className="Cards">
+            <Card titulo="#04 - Desafio Aleatório">
+                <Aleatorio min={5} max={15} />
+            </Card>
 
-        <Card titulo="#02 - Com Parâmetro">
-            <ComParametro
-                titulo="Situação do Aluno"
-                aluno="Pedro Silva"
-                nota={9.3} />
-        </Card>
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
 
-        <Card titulo="#01 - Primeiro Componente">
-            <Primeiro></Primeiro>
-        </Card>
+            <Card titulo="#02 - Com Parâmetro">
+                <ComParametro
+                    titulo="Situação do Aluno"
+                    aluno="Pedro Silva"
+                    nota={9.3} />
+            </Card>
+
+            <Card titulo="#01 - Primeiro Componente">
+                <Primeiro></Primeiro>
+            </Card>
+        </div>
+
     </div>
