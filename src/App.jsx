fdc5afd1565c75eadd762f3c478af83b401043cc
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2021-02-04 13:56:50 -0300

    usando cloneElement

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
 import Familia from './components/basicos/Familia';
+import FamiliaMembro from './components/basicos/FamiliaMembro';
 
 export default () =>
     <div className="App">
@@ -14,7 +15,11 @@ export default () =>
 
         <div className="Cards">
             <Card titulo="#06 - Componente com Filhos" color="#22C8F8">
-                <Familia sobrenome="Ferreira" />
+                <Familia sobrenome="Ferreira">
+                    <FamiliaMembro nome="Pedro" />
+                    <FamiliaMembro nome="Ana" />
+                    <FamiliaMembro nome="Gustavo" />
+                </Familia>
             </Card>
 
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
