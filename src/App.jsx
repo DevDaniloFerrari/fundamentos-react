bc9d58c9c6b620d1b697f9e6dfac1f5e85aea10f
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2021-02-04 13:42:29 -0300

    componente filho

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,17 @@ import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
 import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
+import Familia from './components/basicos/Familia';
 
 export default () =>
     <div className="App">
         <h1>Fundamento React</h1>
 
         <div className="Cards">
+            <Card titulo="#06 - Componente com Filhos" color="#22C8F8">
+                <Familia sobrenome="Ferreira" />
+            </Card>
+
             <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
                 <Aleatorio min={5} max={15} />
             </Card>
